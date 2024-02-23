3f408f30b20c700e9e6950138b0264cba0786458
Alex Moreno <alex.moreno32390@gmail.com>
2024-02-23 12:22:04 +0100

    Ajout de fonctionnalités

--- Calculatrice/calculatrice.js
+++ Calculatrice/calculatrice.js
@@ -1,43 +1,109 @@
-let mult = document.getElementById("multiplication");
-let add = document.getElementById("addition");
-let sub = document.getElementById("soustraction");
-let div = document.getElementById("division");
-let equal = document.getElementById("egal");
+let mult = document.getElementById("calc-multiplication");
+let add = document.getElementById("calc-addition");
+let sub = document.getElementById("calc-soustraction");
+let div = document.getElementById("calc-division");
+let equal = document.getElementById("calc-egal");
 
-let ac = document.getElementById("AC");
+let ac = document.getElementById("utils-ac");
 
 let zoneCalc  = document.getElementById("resultat");
 
 let chiffres = document.querySelectorAll(".chiffre");
 let operat = document.querySelectorAll(".calc");
 
-let calc = "";
-chiffres.forEach((e)=>{
-    e.addEventListener('click', ()=>{
-        calc += e.textContent;
-        console.log(calc);
-    })
-})
+let calc = [];
 
-zoneCalc.add
+let isEmpty = (array)=>{
+    return array.length === 0;
+}
 
-operat.forEach((e)=>{
-    e.addEventListener('click', ()=>{
-        if (e.id === "egal") {
-            if (calc === ""){
-                zoneCalc.innerHTML = "0";
-                console.log("calc vide");
-            }
-            else{
-                let resultat = eval(calc);
-                calc  = "";
-                calc += resultat;
-                console.log(calc);
-            }
+clavier.addEventListener('click', (e)=>{
+    let eTarget = e.target.id.split("-");
+    let type = eTarget[0];
+    let idBtn = eTarget[1];
+
+    let ifLastIsOpeReplace = (elt)=>{
+        if (calc[calc.length-1] === "-" || calc[calc.length-1] === "+" || calc[calc.length-1] === "*" || calc[calc.length-1] === "/") {
+            calc[calc.length-1] = elt;
+            return true;
         }
-        else{
-            calc += e.textContent;
+        return false;
+    }
+
+    switch (type) {
+        case "calc":
+            if (isEmpty(calc)){
+                break;
+            }
+            switch (idBtn) {
+                case "division":
+                    if (ifLastIsOpeReplace("/")){
+                        console.log(calc);
+                        break;
+                    }
+                    calc.push("/");
+                    console.log(calc);
+                    break;
+
+                case "multiplication":
+                    if (ifLastIsOpeReplace("*")){
+                        console.log(calc);
+                        break;
+                    }
+                    calc.push("*");
+                    console.log(calc);
+                    break;
+
+                case "soustraction":
+                    if (ifLastIsOpeReplace("-")){
+                        console.log(calc);
+                        break;
+                    }
+                    calc.push("-");
+                    console.log(calc);
+                    break;
+
+                case "addition":
+                    if (ifLastIsOpeReplace("+")){
+                        console.log(calc);
+                        break;
+                    }
+                    calc.push("+");
+                    console.log(calc);
+                    break;
+                case "egal":
+
+            }
+            break;
+
+        case "utils":
+            switch (idBtn) {
+                case "ac":
+                    calc = [];
+                    ac.textContent = "AC";
+                    zoneCalc.textContent = "0";
+                    console.log(calc);
+                    break;
+            
+                case "plusmoins":
+
+                    break;
+
+                default: //pourcent
+                    break;
+            }
+            break;
+
+        case "ch":
+            if (calc != []) {
+                ac.textContent = "C";
+            }
+            calc.push(e.target.textContent);
             console.log(calc);
-        }
-    })
+            break;
+        
+        default:
+            break;
+
+    }
 })
